a50d51cf6c41271dbbf5ca37f86daa9c046a24fa
rtaugourdeau-LPB <r.taugourdeau@lapremierebrique.fr>
2026-01-20 14:28:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2235,8 +2235,8 @@ def page_docs():
     st.title("📄 Documentation")
 
     DOCS = {
-        "📌 Listing des dashboards PBI": "https://docs.google.com/document/d/129lJRvAsvrxj4P2L_bN5zfhMeF5aXm0YV5wSJ6yLOT8/edit?tab=t.0",
-        "🏗️ Architecture Fabric & MODOP Power BI": "https://docs.google.com/document/d/1MJ7R0YZRqa54vfT9ID8l5upATsqZ39I-82hATI4rCaQ/edit?tab=t.0",
+        "📊 Listing des dashboards PBI": "https://docs.google.com/document/d/129lJRvAsvrxj4P2L_bN5zfhMeF5aXm0YV5wSJ6yLOT8/edit?tab=t.0",
+        "🗄️ Architecture Fabric & MODOP Power BI": "https://docs.google.com/document/d/1MJ7R0YZRqa54vfT9ID8l5upATsqZ39I-82hATI4rCaQ/edit?tab=t.0",
         "🔄 Synchronisation ETL & Alerting emailing": "https://docs.google.com/document/d/1YU93c_Gi0MvFghxcAAd7YXAnriuLxTK_C3lYrJmnhnY/edit?tab=t.0",
     }
     for label, url in DOCS.items():
@@ -2275,6 +2275,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
